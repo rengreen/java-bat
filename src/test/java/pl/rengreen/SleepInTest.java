a7588f0f401862b5a1377e2485b1a1185fefe41a
rengreen <r.m.gardzinska@gmail.com>
2019-10-21 14:37:48 +0200

    Add solution for Warmup-1 sleepIn, problem p187868
    Problem:
    The parameter weekday is true if it is a weekday,
    and the parameter vacation is true if we are on vacation.
    We sleep in if it is not a weekday or we're on vacation.
    Return true if we sleep in.
    
    from https://codingbat.com/prob/p187868

---
+++ src/test/java/pl/rengreen/SleepInTest.java
@@ -0,0 +1,38 @@
+package pl.rengreen;
+
+/*
+sleepIn(false, false) → true
+sleepIn(true, false) → false
+sleepIn(false, true) → true
+sleepIn(true, true) → true
+*/
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SleepInTest {
+
+    private SleepIn sleepIn = new SleepIn();
+
+    @Test
+    public void ShouldReturnTrue_WhenNotWeekdayAndNotVacation() {
+        assertTrue(sleepIn.sleepIn(false, false));
+    }
+
+    @Test
+    public void ShouldReturnFalse_WhenWeekdayAndNotVacation() {
+        assertFalse(sleepIn.sleepIn(true, false));
+    }
+
+    @Test
+    public void ShouldReturnTrue_WhenNotWeekdayAndVacation() {
+        assertTrue(sleepIn.sleepIn(false, true));
+    }
+
+    @Test
+    public void ShouldReturnTrue_WhenWeekdayAndVacation() {
+        assertTrue(sleepIn.sleepIn(true, true));
+    }
+}
